93bd69d15f250f539b43c435d8db9462e182df2d
eli-atlanta <vassilievilia@gmail.com>
2024-01-20 17:01:59 +0700

    test 7

---
+++ input_and_output/5_from_json_file.py
@@ -0,0 +1,20 @@
+import json
+
+def from_json_file(file_name):
+    with open(file_name, "r") as open_file:
+        return json.load(open_file)
+
+
+if __name__ == '__main__':
+    import os
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_name = f"{dir_path}/data.json"
+
+    data_object = from_json_file(file_name)
+
+    print(data_object)
+    print(type(data_object))
+    print()
+    for key, val in data_object.items():
+        print(f"{key}: {type(val).__name__}: {val}")
