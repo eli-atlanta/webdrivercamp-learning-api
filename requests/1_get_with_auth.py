0c80795ace9ff084c2089bb9ac7283c808a7da0c
eli-atlanta <vassilievilia@gmail.com>
2024-01-22 17:13:54 +0700

    test 10

---
+++ requests/1_get_with_auth.py
@@ -0,0 +1,12 @@
+import requests
+
+def get_with_auth(url):
+    #YOUR CODE HERE
+
+if __name__=="__main__":
+    url = "https://api.github.com/user/repos"
+
+    num_of_repos, headers = get_with_auth(url)
+
+    print(f"Total Repos: {num_of_repos}")
+    print(f"Response headers: {headers}")
