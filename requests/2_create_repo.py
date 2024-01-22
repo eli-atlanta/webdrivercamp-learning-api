84c1a90e0d0e249e25bc75c7ebf70d9b1b5fc868
eli-atlanta <vassilievilia@gmail.com>
2024-01-23 05:41:35 +0700

    test 12

---
+++ requests/2_create_repo.py
@@ -0,0 +1,17 @@
+import requests
+from pprint import pprint
+
+def create_repo(url):
+    header_authorization = {'Authorization': 'token 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'}
+    myobj = {'name': 'repo-created-with-api', 'private': 'True', 'has_wiki': 'False'}
+    r = requests.post(url, headers=header_authorization, json=myobj)
+
+    print(f"Response status code: {r.status_code}")
+    return r.json()
+
+
+if __name__=='__main__':
+    url = 'https://api.github.com/user/repos'
+
+    repo = create_repo(url)
+    pprint(repo)
